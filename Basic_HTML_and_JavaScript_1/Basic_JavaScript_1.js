3c58b08f7a19032b3557590d0b5b0961594cdbe5
BBun-bun <1anthony.mds@gmail.com>
2023-06-06 16:35:44 +0100

    fixed being unable to open the file on git hub by deleting a hidden file called git attributes

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+function Slumby(){
+	alert("he slumby");
+}
+
+function ChangeText(){
+	btn.textContent = 'He slumby'
+	document.getElementById("btn").innerHTML = 
+	result;
+}
+
+window.onload=Slumby;
